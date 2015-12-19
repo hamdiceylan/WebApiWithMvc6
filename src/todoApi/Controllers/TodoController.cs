8cecfe0d28902e3e9c544448c80aa1ab0d3d98d0
hamdiceylan <hamdiceylan@gmail.com>
2015-12-19 04:55:39 +0200

    first commit

---
+++ src/todoApi/Controllers/TodoController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using todoApi.Model;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace todoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        [FromServices]
+        public ITodoRepository TodoItems { get; set; }
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoItems.GetAll();
+        }
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = TodoItems.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return new ObjectResult(item);
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return HttpBadRequest();
+            }
+            TodoItems.Add(item);
+            return CreatedAtRoute("GetTodo", new { controller = "Todo", id = item.Key }, item);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return HttpBadRequest();
+            }
+
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+
+            TodoItems.Update(item);
+            return new NoContentResult();
+        }
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            TodoItems.Remove(id);
+        }
+    }
+}
---
+++ src/todoApi/Model/todoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todoApi.Model
+{
+    public class TodoItem
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
